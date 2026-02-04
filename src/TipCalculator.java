e8a8f5a2d4f56533c01463e914fb0d7ece63ba50
James Kollilon Barclay III <jkbarclay261@gmail.com>
2026-02-04 18:04:04 -0500

    "Added methods"

---
+++ src/TipCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class tipCalculator {
+
+    public static double calculateTax(double taxRate){
+     return  taxRate/100;
+
+    }
+    public static double calculateTip(double rawBill)
+
+
+
+ public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+     System.out.println("Enter Bill amount: ");
+     double rawBill = Double.parseDouble(scanner.nextLine());
+
+     System.out.println("Enter Tax rate %: ");
+     double taxRate = Double.parseDouble(scanner.nextLine());
+
+     System.out.println("Enter Tip rate:");
+     double tipRate = Double.parseDouble(scanner.nextLine());
+
+
+
+
+
+}
+
+
+
+
+
+}
