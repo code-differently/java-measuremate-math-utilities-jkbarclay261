a5400392f6bd64818087b00d56590c59529b08b3
James Kollilon Barclay III <jkbarclay261@gmail.com>
2026-02-04 20:51:13 -0500

    "Completed Tip Calculator!!"

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -3,12 +3,10 @@ class tipCalculator {
 
     public static double calculateTax(double taxRate){
      return  taxRate/100;
-
     }
     public static double calculateTip(double rawBill, double tipRate){
         return rawBill * (tipRate/100);
     }
-
     public static double calculateTotalBill(double rawBill, double taxAmount,double tipAmount){
         return rawBill + taxAmount + tipAmount;
     }
@@ -16,8 +14,11 @@ class tipCalculator {
         return Math.round(totalBill);
     }
 
+
+
  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    //Creating my scanner
 
      System.out.println("Enter Bill amount: ");
      double rawBill = Double.parseDouble(scanner.nextLine());
@@ -28,15 +29,20 @@ class tipCalculator {
      System.out.println("Enter Tip rate:");
      double tipRate = Double.parseDouble(scanner.nextLine());
 
+    //Calling my method s to my main
+     double taxAmount = calculateTax(taxRate);
 
-     //Object calculateRoundedTotalBill = null;
-     //System.out.println(Long calculateRoundedTotalBill);
+     double tipAmount = calculateTip(rawBill, tipRate);
 
+     double totalBill = calculateTotalBill(rawBill, taxAmount, tipAmount);
+
+     long roundedTotalBill = calculateRoundedTotalBill(totalBill);
+
+     //Output results
+     System.out.println("Tax Amount: " + taxAmount);
+     System.out.println("Tip Amount: " + tipAmount);
+     System.out.println("Total Bill (Raw): " + totalBill);
+     System.out.println("Total Bill (Rounded): " + roundedTotalBill);
 
 }
-
-
-
-
-
 }
